70bb9be0ca423cdf42373ce351bb9a70df4ee726
Atsushi Nagase <a@ngs.io>
2019-03-20 22:16:15 +0900

    Update OKI driver

--- Casks/oki-hiper-c-driver.rb
+++ Casks/oki-hiper-c-driver.rb
@@ -1,11 +1,11 @@
 cask 'oki-hiper-c-driver' do
-  version '2.0.0'
-  sha256 '5cc1eb9f2717f924248320a1217309cf93fe2350850bad72e3c853ecb46ca305'
+  version '2.0.5'
+  sha256 '837ee5f4539f63728de2663fdace584a8c1a2d3de75029b65f147fcb4facfdb5'
 
-  url 'http://www5.okidata.co.jp/JSHIS163.nsf/SearchView/7BF3B295CBFB44F349257ACC00285CAE/$File/OKI_MXMLion_DHC_ww_A1.dmg'
+  url 'https://www.oki.com/jp/printing/download/C511_C312_C301_HC_OSX_020005_132163.dmg'
   name 'OKI C511dn/C312dn/C301dn Printer Driver'
-  homepage 'http://www5.okidata.co.jp/JSHIS163.nsf/SearchView/7BF3B295CBFB44F349257ACC00285CAE?OpenDocument&charset=Shift_JIS'
+  homepage 'https://www.oki.com/jp/printing/support/drivers-and-utilities/?id=46259001FZ01&tab=drivers-and-utilities&productCategory=color&sku=C511DN&os=ab53&lang=ac1'
 
-  pkg 'Installer for OS X.pkg'
+  pkg 'Installer for OSX10.5-10.12.pkg'
   uninstall :pkgutil => 'com.okidata.hipercDriver.*'
 end
